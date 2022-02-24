e0644295cd706acac8258287e8b24687d17996e9
Gabriel Munhoz <gabrielsds.munhoz@hotmail.com>
2022-02-23 21:06:08 -0300

    teste

--- main.js
+++ main.js
@@ -17,4 +17,14 @@ function changeSocialMediaLinks(){
 
 function getGitHubProfileInfos(){
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url) 
+  .then(response =>  response.json() )
+  .then(data => { 
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+
+   })
 }
+
+getGitHubProfileInfos()
