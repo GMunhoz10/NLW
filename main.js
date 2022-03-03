46bef71d86eb1749eba4665f97d8ceaa44cc5eb2
Gabriel Munhoz <gabrielsds.munhoz@hotmail.com>
2022-03-03 20:51:28 -0300

    att

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
 const LinksSocialMedia = {
-
-  github = "GMunhoz10",
-  facebook = "gabrielsds.munhoz",
-  instagram = "gmunhozzz__"
-
+  github: 'GMunhoz10',
+  facebook: 'gabrielsds.munhoz',
+  instagram: 'gmunhozzz__'
 }
 
-function changeSocialMediaLinks(){
+function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
 
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
   }
-
 }
 
-function getGitHubProfileInfos(){
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
-  fetch(url) 
-  .then(response =>  response.json() )
-  .then(data => { 
-    userName.textContent = data.name
-    userBio.textContent = data.bio
+  console.log(url)
 
-   })
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
 }
 
 getGitHubProfileInfos()
